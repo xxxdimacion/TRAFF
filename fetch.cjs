2bcccbed33ba58616d5b6c1caeff256033d61e24
xxxdimacion <109275809+xxxdimacion@users.noreply.github.com>
2026-03-26 18:09:50 +0300

    Initial commit

--- fetch.cjs
+++
@@ -1,6 +0,0 @@
-const https = require('https');
-https.get('https://www.brezmarketing.net/', (res) => {
-  let data = '';
-  res.on('data', (chunk) => { data += chunk; });
-  res.on('end', () => { console.log(data); });
-}).on('error', (err) => { console.log("Error: " + err.message); });
